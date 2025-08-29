6f2c71413b0be3143966e5e9566cbd1ee6589c73
PogozhevIvan18 <pogozhev_reserv@mail.ru>
2025-08-29 23:04:45 +0400

    client-2

---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,59 @@
+import {makeAutoObservable} from 'mobx';
+
+export default class DeviceStore {
+    constructor() {
+        this._types = [
+            {id: 1, name: "Холодильники"},
+            {id: 2, name: "Смартфоны"},
+            {id: 3, name: "Ноутбуки"},
+            {id: 4, name: "Телевизоры"}
+        ]
+        this._brands = [
+            {id: 1, name: "Sumsung"},
+            {id: 2, name: "Apple"},
+            {id: 3, name: "Lenovo"}
+        ]
+        this._devices = [
+            {id: 1, name: "iPhone 12 Pro", price: 25000, rating: 5, img: ""},
+            {id: 2, name: "iPhone 12 Pro", price: 25000, rating: 5, img: ""},
+            {id: 3, name: "iPhone 12 Pro", price: 25000, rating: 5, img: ""},
+            {id: 4, name: "iPhone 12 Pro", price: 25000, rating: 5, img: ""}
+        ]
+        this._selectedType = {}
+        this._selectedBrand = {}
+        makeAutoObservable(this)
+    }
+
+    setTypes(types) {
+        this._types = types
+    }
+    setBrands(brands) {
+        this._brands = brands
+    }
+    setDevices(devices) {
+        this._devices = devices
+    }
+
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
+
+    get types() {
+        return this._types
+    }
+    get brands() {
+        return this._brands
+    }
+    get devices() {
+        return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
+    }
+    get selectedBrand() {
+        return this._selectedBrand
+    }
+}
